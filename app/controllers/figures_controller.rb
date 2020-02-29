9cb0badd49f66bded86fe1baee6c60e8499425fe
reddevilcero <reddevil_cero@hotmail.com>
2020-02-29 11:24:27 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,12 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+
+    erb :'figures/index'
+  end
+  get '/figures/new' do
+
+    erb :'figures/new'
+  end
 end
