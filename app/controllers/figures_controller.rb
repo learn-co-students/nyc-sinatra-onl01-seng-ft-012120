2084a7265d7318b68363bbb2a4c8b001faef42c7
kellypowers <kpowers.nj@gmail.com>
2020-03-02 10:23:32 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,79 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/' do 
+    redirect to "/figures"
+  end
+
+  get '/figures' do 
+    @figures = Figure.all 
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+
+  get '/figures/:id' do 
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    @titles = Title.all 
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
+
+
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+
+    if !params["landmark"]["name"].empty?
+      #landmark1 = Landmark.find_or_create(params["landmark"])
+      #@figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params["title"]["name"].empty?
+      #title1 = Title.find_or_create_by(name: params["title"]["name"])
+      #@figure.titles << Title.create(name: params["title"]["name"])
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
+
+  patch "/figures/:id" do 
+    @figure = Figure.find_by_id(params[:id])
+
+
+    @figure.update(params[:figure])
+    if !params["landmark"]["name"].empty?
+      #landmark1 = Landmark.find_or_create(params["landmark"])
+      #@figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params["title"]["name"].empty?
+      #title1 = Title.find_or_create_by(name: params["title"]["name"])
+      #@figure.titles << Title.create(name: params["title"]["name"])
+      @figure.titles << Title.create(params[:title])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  delete "figures/:id" do 
+    Figure.destroy(params[:id])
+    redirect to "/figures"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,54 @@
 class LandmarksController < ApplicationController
   # add controller methods
+    # add controller methods
+    
+  
+    get '/landmarks' do 
+      @landmarks = Landmark.all 
+      figures = Figure.all
+      erb :'landmarks/index'
+    end
+  
+    get '/landmarks/new' do 
+      erb :'landmarks/new'
+    end
+
+    get '/landmarks/:id' do 
+      @landmark = Landmark.find_by_id(params[:id])
+  
+      erb :'landmarks/show'
+    end
+
+
+    get '/landmarks/:id/edit' do 
+      @landmark = Landmark.find_by_id(params[:id])
+  
+      erb :"landmarks/edit"
+    end
+  
+  
+    post '/landmarks' do 
+      @landmark = Landmark.create(params[:landmark])
+      
+      redirect to "/landmarks"
+    end
+  
+  
+  
+    patch "/landmarks/:id" do 
+      @landmark = Landmark.find(params[:id])
+  
+  
+      @landmark.update(params[:landmark])
+      redirect to "/landmarks/#{@landmark.id}"
+    end
+
+  
+    delete "landmarks/:id" do 
+      landmark.destroy(params[:id])
+      redirect to "/landmarks"
+    end
+  
+  
+  
 end
